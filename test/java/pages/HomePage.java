adddd36a32b281db43f2a94c5e54daa983fab3ce
Sergii Opanasiuk <sergii.opanasiuk@gmail.com>
2018-07-03 20:17:44 -0500

    #666: More page objects

--- test/java/pages/HomePage.java
+++ test/java/pages/HomePage.java
@@ -5,7 +5,14 @@ public class HomePage {
     @FindBy(Name = "firstName")
     private WebElement firstname;
 
+    @FindBy(Name = "firstName")
+    private WebElement lastName;
+
     public void setFirstname(String name){
+        firstname.setValue(name);
+    }
+
+    public void setLastNametname(String name){
         firstname.setValue(name);
     }
 
